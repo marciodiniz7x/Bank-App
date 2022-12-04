e58c0cb487b90686c414bc48a8e37bf09d7c9bbe
Márcio Diniz <marcio.sccp10@gmail.com>
2022-12-04 20:11:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 // Seletor de elementos
-data = document.querySelector('.data');
+const data = document.querySelector('.data');
+
+
+//////////////////
+///// Script /////
+
+// Datas
+const pegaData = new Date();
+const diaSemana = pegaData.getDay();
+// método slice para adicionar um 0 quando o número tiver apenas 1 dígito
+const diaMes = ("00" + pegaData.getDate()).slice(-2);
+//
+const mes = pegaData.getMonth();
+
+const meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+
+const semana = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"]
+
+data.innerHTML = `${semana[diaSemana]}, ${diaMes} `
