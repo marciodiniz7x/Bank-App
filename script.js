d25db0a9c4e79827e4453284d65a235823830098
Márcio Diniz <marcio.sccp10@gmail.com>
2022-12-17 11:50:19 -0300

    Atts

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Seletor de elementos
 const data = document.querySelector('.data');
-
+const hora = document.querySelector('.hora');
+const hora2 = document.querySelector('.hora2');
 
 //////////////////
 ///// Script /////
@@ -11,6 +12,7 @@ const diaSemana = pegaData.getDay();
 // método slice para adicionar um 0 quando o número tiver apenas 1 dígito
 const diaMes = ("00" + pegaData.getDate()).slice(-2);
 //
+
 const mes = pegaData.getMonth();
 const meses = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'];
 
@@ -18,4 +20,11 @@ const ano = pegaData.getFullYear()
 
 const semana = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"];
 
-data.innerHTML = `${semana[diaSemana]}, ${diaMes} ${meses[mes]}, ${ano} `
+// Relógio
+const pegaHora = ("00" + pegaData.getHours()).slice(-2);
+const pegaMinuto = ("00" + pegaData.getMinutes()).slice(-2);
+
+//Substituição dos valores
+data.innerHTML = `${semana[diaSemana]}, ${diaMes} ${meses[mes]}, ${ano} `;
+hora.innerHTML = `${pegaHora}:${pegaMinuto}`;
+hora2.innerHTML = `${pegaHora}:${pegaMinuto}`;
