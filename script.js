ed9088a24927a98cc5c8c7dbaddba85cde43b9cb
Márcio Diniz <marcio.sccp10@gmail.com>
2022-12-29 22:18:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,6 @@ hora2.innerHTML = `${pegaHora}:${pegaMinuto}`;
 nomeSobreposto.addEventListener('focusin', () => {
     nome.classList.add('sumir');
     nomeSobreposto.classList.remove('sumir');
-
 });
 
 nomeSobreposto.addEventListener('focusout', () => {
